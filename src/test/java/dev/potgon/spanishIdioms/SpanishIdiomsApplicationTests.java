d4039bf71503d194c34b4b19675406aa6d3144ed
potgon <otgon.petru@gmail.com>
2024-05-31 09:31:31 +0200

    Test folder

---
+++ src/test/java/dev/potgon/spanishIdioms/SpanishIdiomsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.potgon.spanishIdioms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpanishIdiomsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
