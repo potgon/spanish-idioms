8e7323c1eba48074729085e850b5d1f156b8ba4b
potgon <otgon.petru@gmail.com>
2024-05-31 09:29:52 +0200

    Spring app

---
+++ src/main/java/dev/potgon/spanishIdioms/SpanishIdiomsApplication.java
@@ -0,0 +1,13 @@
+package dev.potgon.spanishIdioms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpanishIdiomsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpanishIdiomsApplication.class, args);
+	}
+
+}
